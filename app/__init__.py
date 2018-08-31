27e6b2f6438c22a04d0b845e7ce3890ad0da866d
chemf <eoyohe@gmail.com>
2018-08-31 17:58:51 +0800

    [IMP] signature valid

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,20 @@
+from wechatpy.utils import check_signature
 from flask import Flask, request
+from wechatpy.exceptions import InvalidSignatureException
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('config.py')
 
 @app.route('/')
-def hello():
-    return app.config['TOKEN']
+def message():
+    token = app.config['TOKEN']
+    nonce = request.args.get('nonce')
+    signature = request.args.get('signature')
+    timestamp = request.args.get('timestamp')
+    echostr = request.args.get('echostr')
+    try:
+        check_signature(token, signature, timestamp, nonceo)
+        return echostr
+    except InvalidSignatureException as e:
+        print(e)
 
